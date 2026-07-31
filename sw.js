1379c7d1d4ded3e98303ab948602831cb539df45
giftedu101-design <giftedu101@pen.go.kr>
2026-07-31 09:12:59 +0900

    Refresh inspired mobile app assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="badamoa-mobile-v5";
-const APP_FILES=["./","./?mode=demo","./index.html","./manifest.webmanifest","./assets/app-icon.svg","./css/styles.css","./css/recommendation.css","./css/mobile-pwa.css","./css/reference-inspired.css","./js/data-facilities.js","./js/storage.js","./js/app-layout.js"];
+const CACHE_NAME="badamoa-mobile-v6";
+const APP_FILES=["./","./?mode=demo","./index.html","./manifest.webmanifest","./assets/app-icon.svg","./css/styles.css","./css/recommendation.css","./css/mobile-pwa.css","./css/reference-inspired.css","./js/data-facilities.js","./js/storage.js","./js/app-inspired.js"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./?mode=demo"))))});
